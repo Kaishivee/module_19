fe3c03c4211a2adc1db4aa5e31baa816c38daead
User <dasha.ignatova.99@bk.ru>
2024-10-19 18:12:25 +0300

    new commit!

--- game_store/task1/admin.py
+++ game_store/task1/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Game, Buyer
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'description', 'age_limited')
+    search_fields = ('title',)
+    list_filter = ('age_limited', 'cost',)
+    fieldsets = (
+        ('Информация об игре', {'fields': (('age_limited', 'title'), ('cost', 'size'))}),
+        ('Дополнительное', {'fields': ('description',)}),
+    )
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    fieldsets = (
+        ('Информация о покупателе', {'fields': (('name',), ('balance', 'age'))}),
+        ('Дополнительное', {'fields': ()}),
+    )
+
